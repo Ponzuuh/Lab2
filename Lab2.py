8094d7ce985c70dc3f78fde5e6dbaa275a46d5a5
Yuan_Ming <yuanming.22@ichat.sp.edu.sg>
2023-04-27 12:39:23 +0800

    Median

--- Lab2.py
+++ Lab2.py
@@ -17,15 +17,27 @@ def find_min_max(nom):
     min_num = min(nom)
     max_num = max(nom)
     return [min_num, max_num]
-def sort_temperature():
-    print()
-def calc_median_temperature():
-    print()
+def sort_temperature(nom):
+    sorted_temp = sorted(nom)
+    n = len(sorted_temp)
+    return sorted_temp
+
+def calc_median_temperature(n, sorted_temp):
+    mid_num = n // 2
+    if n % 2 == 0:
+        median = (sorted_temp[mid_num + 1] + sorted_temp[mid_num])/2
+    else:
+        median = sorted_temp[mid_num]
+    return median
 
 nom = get_user_input()
 avg_temp = calc_average(nom)
 min_max = find_min_max(nom)
+sorted_temp = sort_temperature(nom)
+n = len(nom)
+median = calc_median_temperature(n, sorted_temp)
 
 print('Average: ', avg_temp)
 print('Minimum: ', min_max[0])
 print('Maximum: ', min_max[1])
+print("Median: ", median)
